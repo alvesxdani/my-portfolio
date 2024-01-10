955271f479f70b97d0ff875bb5c8f8fde351cb46
alvesxdani <dani.alvesm22@gmail.com>
2024-01-10 16:57:27 -0300

    :bug: fix: section responsivity

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -6,7 +6,7 @@ const Sobre = () => {
   return (
     <section
       id="sobre"
-      className="flex justify-center items-center align-middle w-100 min-h-[90vh] relative bg-[#FDFBFB]"
+      className="flex justify-center items-center align-middle w-100 h-auto relative bg-[#FDFBFB] sm:min-h-[90vh]"
     >
       <div className="flex flex-col justify-center items-center gap-10 md:flex-row">
         <Image src="/Images/Sobre/foto.png" alt="" width={200} height={200} />
