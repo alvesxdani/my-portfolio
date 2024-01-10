9c66736e07ec60eb9023bad42ce32695ab3a4009
alvesxdani <dani.alvesm22@gmail.com>
2024-01-10 02:08:43 -0300

    :lipstick: Add link to my cv em download button

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -8,22 +8,34 @@ const Sobre = () => {
       id="sobre"
       className="flex justify-center items-center align-middle w-100 min-h-[90vh] relative bg-[#FDFBFB]"
     >
-        <div className='flex justify-center items-center gap-10'>
+      <div className="flex justify-center items-center gap-10">
         <Image src="/Images/Sobre/foto.png" alt="" width={200} height={200} />
 
-<div className="flex flex-col justify-center items-center w-[50%] gap-6">
-  <h2 className="font-montserrat font-bold text-center text-maincolor">
-    Sobre
-  </h2>
-  <p className='text-justify'>
-  Sou uma desenvolvedora front-end com mais de um ano de experiência, especializada em React, TypeScript e Next.js. Tenho conhecimentos em Figma e priorizo códigos reutilizáveis, sempre buscando aderir aos princípios de Clean code e Clean architecture. Estou comprometida em continuar aprimorando minhas habilidades e contribuir para projetos desafiadores.
-  </p>
-  <Button size='lg' type='button'>
-  <Image src="/Images/Sobre/download.png" alt="" width={18} height={24} />
-    Download CV
-  </Button>
-</div>
+        <div className="flex flex-col justify-center items-center w-[50%] gap-6">
+          <h2 className="font-montserrat font-bold text-center text-maincolor">
+            Sobre
+          </h2>
+          <p className="text-justify">
+            Sou uma desenvolvedora front-end com mais de um ano de experiência,
+            especializada em React, TypeScript e Next.js. Tenho conhecimentos em
+            Figma e priorizo códigos reutilizáveis, sempre buscando aderir aos
+            princípios de Clean code e Clean architecture. Estou comprometida em
+            continuar aprimorando minhas habilidades e contribuir para projetos
+            desafiadores.
+          </p>
+          <a href="https://www.canva.com/design/DAE_M3zLeaQ/scdLlFZRwZoNEXjHB23DuQ/view?utm_content=DAE_M3zLeaQ&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#1" target='_blank'>
+            <Button size="lg" type="button">
+              <Image
+                src="/Images/Sobre/download.png"
+                alt=""
+                width={18}
+                height={24}
+              />
+              Download CV
+            </Button>
+          </a>
         </div>
+      </div>
     </section>
   )
 }
