48372fe81bfb3da86fd1041b544edbdfadcb995b
alvesxdani <dani.alvesm22@gmail.com>
2024-01-12 14:10:12 -0300

    :sparkles: feat: finish inputs components and contact section

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Input, TextArea } from "../Inputs";
+import emailjs from "@emailjs/browser";
+import Button from "../Button";
 
 const Contato = () => {
+//   const form: React.MutableRefObject<HTMLFormElement> = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//       .sendForm(
+//         "YOUR_SERVICE_ID",
+//         "YOUR_TEMPLATE_ID",
+//         form.current,
+//         "YOUR_PUBLIC_KEY"
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
   return (
-    <>
-    <div className='flex flex-col justify-center items-center align-middle w-100 min-h-[90vh] gap-[5rem] p-5 bg-[#FDFBFB]'>
-        <h2 className="font-montserrat font-bold text-center text-maincolor">
+    <div className="flex flex-col justify-center items-center align-middle w-100 min-h-[90vh] gap-[5rem] p-5">
+      <h2 className="font-montserrat font-bold text-center text-maincolor">
         Contato
       </h2>
 
-      <div className="flex">
-        <form>
-            
+      <div className="flex flex-col-reverse md:flex-row w-[100%] gap-10">
+        <form className="w-[100%] md:w-[50%] border">
+          <Input type="text" id="name" label="Nome"/>
+          <Input type="email" id="email" label="E-mail"/>
+          <TextArea label="Mensagem"/>
+          <Button size="md" type="submit">
+            Enviar
+          </Button>
         </form>
-        <div>Teste</div>ssss
+        <div className="w-[100%] md:w-[50%] border">
+            Teste
+        </div>
       </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Contato
+export default Contato;
