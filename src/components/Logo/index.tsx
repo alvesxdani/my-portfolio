84b4beb9beacc069bb60ee15a1b555c3113fd14c
alvesxdani <dani.alvesm22@gmail.com>
2024-01-12 23:48:26 -0300

    :sparkles: feat: add logo component and apply it on header

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="font-montserrat text-white text-4xl font-bold">
+      D<span className="text-maincolor">.</span>
+    </div>
+  )
+}
+
+Logo.propTypes = {}
+
+export default Logo
