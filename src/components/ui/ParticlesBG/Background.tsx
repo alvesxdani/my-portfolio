74377b3344832415c106564f89380b7f638f0bd1
alvesxdani <dani.alvesm22@gmail.com>
2024-02-17 16:40:56 -0300

    :sparkles: add particles bg

---
+++ src/components/ui/ParticlesBG/Background.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useMemo, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import {
+  type Container,
+  type ISourceOptions,
+} from '@tsparticles/engine'
+import { loadSlim } from '@tsparticles/slim' // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { configParticles } from './config'
+
+
+const Background = () => {
+  const [init, setInit] = useState(false)
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine)
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true)
+    })
+  }, [])
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container)
+  }
+
+  const options: ISourceOptions = useMemo(
+    () => (configParticles),
+    [],
+  )
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+        className='border'
+      />
+    )
+  }
+
+  return null
+}
+
+export default Background
