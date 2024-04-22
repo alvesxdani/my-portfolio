ea55875ea4c3e88df17d86f6f986a0bc46ed12b6
alvesxdani <dani.alvesm22@gmail.com>
2024-04-22 17:51:59 -0300

    :card_file_box: adicionando mais um projeto ao JSON

--- src/components/Projetos/projetos-array.tsx
+++ src/components/Projetos/projetos-array.tsx
@@ -1,5 +1,13 @@
 export const ProjetosLista = [
   {
+    title: 'Agenda de contatos',
+    subtitle:
+      'React - Typescript - Redux - Firebase - Firestore - Yup - React Hook Form - Styled-Components - React-Router-Dom',
+    text: 'Este é o projeto de uma agenda de contatos telefônicos, onde você realiza o login, visualiza os contatos salvos, edita, atualiza e remove.',
+    github: 'https://github.com/alvesxdani/lista-contatos',
+    deploy: 'https://lista-contatos-pied.vercel.app/',
+  },
+  {
     title: 'Desafio Técnico 1',
     subtitle: 'Next - TypeScript - TailwindCSS - Emailjs - Dark Mode',
     text: 'Este é o projeto de um desafio que realizei para um teste técnico, uma aplicação web desenvolvida utilizando Next.js, TypeScript, Tailwind CSS e outras tecnologias. Possui validação básica de formulário, animações e dark mode',
@@ -9,13 +17,14 @@ export const ProjetosLista = [
   {
     title: 'Rede social',
     subtitle: 'React - ContextAPI - Validação de Token',
-    text: 'Projeto criado no curso de React da Origamid. Consiste em uma rede social de cães, onde você se cadastra, realiza o login e realiza postagens com a foto do cão, nome, peso e idade, como também pode realizar comentários nas postagens.',
+    text: 'Este é um projeto criado no curso de React da Origamid. Consiste em uma rede social de cães, onde você se cadastra, realiza o login e realiza postagens com a foto do cão, nome, peso e idade, como também pode realizar comentários nas postagens.',
     github: 'https://github.com/alvesxdani/social-network-dogs',
     deploy: 'https://social-network-dogs-two.vercel.app/',
   },
   {
     title: 'Cartão interativo',
-    subtitle: 'React - Typescript - Styled Components - React-Hook-Forms - Yup - React-Text-Mask',
+    subtitle:
+      'React - Typescript - Styled Components - React-Hook-Forms - Yup - React-Text-Mask',
     text: 'Este é um projeto de uma página interativa, onde o usuário coloca seus dados do cartão, onde aparecerá na tela. O projeto é um desafio do Front End Mentor.',
     github: 'https://github.com/alvesxdani/interactive-card',
     deploy: 'https://alvesxdani.github.io/interactive-card/',
