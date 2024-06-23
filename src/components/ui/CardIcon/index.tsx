ce5084f010137650d57c0a55994877511328dbef
alvesxdani <dani.alvesm22@gmail.com>
2024-06-23 16:24:44 -0300

    :sparkles: feat: creating a new card component to skills section

--- src/components/ui/CardIcon/index.tsx
+++ src/components/ui/CardIcon/index.tsx
@@ -3,17 +3,20 @@ import { EffectCards } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/effect-cards'
 import './style.css'
-import { ReactNode } from 'react'
+import { ReactElement, ReactNode } from 'react'
 
 type Props = {
-  icons: ReactNode[]
+  icons: ReactElement[]
 }
 
 const CardIcon = ({icons}: Props) => {
   return (
     <Swiper effect={'cards'} modules={[EffectCards]} className="mySwiper">
       {icons.map((item, i) => (
-        <SwiperSlide key={i}>{item}</SwiperSlide>
+        <SwiperSlide key={i}>
+          {item}
+          <span className='text-sm font-lato'>{item.key}</span>
+          </SwiperSlide>
       ))}
     </Swiper>
   )
